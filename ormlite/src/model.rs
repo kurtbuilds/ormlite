99b338e02e9311e085254f0e5838a7efccfed9ba
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-12-27 19:07:41 -0500

    add deprecating marking

--- ormlite/src/model.rs
+++ ormlite/src/model.rs
@@ -5,3 +5,6 @@ pub use ormlite_core::SelectQueryBuilder;
 pub use sqlx::sqlx_macros::FromRow;
 pub use ormlite_core::model::Model;
 pub use ormlite_macro::Model;
+
+#[deprecated(note = "This trait is part of ormlite::Model now.")]
+pub trait HasQueryBuilder {}
