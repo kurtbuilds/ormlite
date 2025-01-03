1009e7cf38ddbb0c41c420df8a9c125be3182f00
Kurt Wolf <kurtwolfbuilds@gmail.com>
2025-01-03 08:18:17 -0500

    fix tests

--- core/src/insert.rs
+++ core/src/insert.rs
@@ -1,7 +1,7 @@
 use crate::Result;
 use futures::future::BoxFuture;
 pub use sqlmo::query::OnConflict;
-use sqlmo::{Dialect, Insert, ToSql, Union, Select, Cte, SelectColumn};
+use sqlmo::{Dialect, Insert, ToSql};
 
 /// Represents an insert query.
 /// We had to turn this into a model because we need to pass in the on_conflict configuration.
@@ -29,34 +29,35 @@ impl<'a, Acquire, Model: crate::model::Model<DB>, DB: sqlx::Database> std::futur
     fn into_future(self) -> Self::IntoFuture {
         // hack to get around the fact that postgres drops the return
         // value in ON CONFLICT DO NOTHING case
-        let q = if matches!(self.insert.on_conflict, OnConflict::Ignore) {
-            let insert_as_select = Select {
-                ctes: vec![
-                    Cte::new("inserted", self.insert)
-                ],
-                columns: vec![SelectColumn::raw("*")],
-                from: Some("inserted".into()),
-                ..Select::default()
-            };
-            let pkey = Model::primary_key().unwrap();
-            let plc_idx = Model::primary_key_placeholder_idx().unwrap();
-            let select_existing = Select {
-                from: Some(Model::table_name().into()),
-                columns: Model::table_columns().iter().map(|&c| c.into()).collect(),
-                where_: format!("{pkey} = ${plc_idx}").into(),
-                ..Select::default()
-            };
-            let union = Union {
-                all: true,
-                queries: vec![
-                    insert_as_select,
-                    select_existing
-                ]
-            };
-            union.to_sql(Dialect::Postgres)
-        } else {
-            self.insert.to_sql(Dialect::Postgres)
-        };
+        // let q = if matches!(self.insert.on_conflict, OnConflict::Ignore) {
+        //     let insert_as_select = Select {
+        //         ctes: vec![
+        //             Cte::new("inserted", self.insert)
+        //         ],
+        //         columns: vec![SelectColumn::raw("*")],
+        //         from: Some("inserted".into()),
+        //         ..Select::default()
+        //     };
+        //     let pkey = Model::primary_key().unwrap();
+        //     let plc_idx = Model::primary_key_placeholder_idx().unwrap();
+        //     let select_existing = Select {
+        //         from: Some(Model::table_name().into()),
+        //         columns: Model::table_columns().iter().map(|&c| c.into()).collect(),
+        //         where_: format!("{pkey} = ${plc_idx}").into(),
+        //         ..Select::default()
+        //     };
+        //     let union = Union {
+        //         all: true,
+        //         queries: vec![
+        //             insert_as_select,
+        //             select_existing
+        //         ]
+        //     };
+        //     union.to_sql(Dialect::Postgres)
+        // } else {
+        // self.insert.to_sql(Dialect::Postgres)
+        // };
+        let q = self.insert.to_sql(Dialect::Postgres);
         (self.closure)(self.acquire, self.model, q)
     }
 }
